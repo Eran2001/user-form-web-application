dff615ad51ec2c9b9578674016a3b09191465bee
Eran Hasareli <126740413+Eran2001@users.noreply.github.com>
2026-02-10 00:07:20 +0530

    Add support for TypeScript configuration file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// vite.config.ts
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
