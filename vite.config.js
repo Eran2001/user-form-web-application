4bf050e6b1846feb7bd5327838b8c823e5e680ca
Eran Hasareli <126740413+Eran2001@users.noreply.github.com>
2026-02-21 10:10:46 +0530

    Add comment to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+//
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
